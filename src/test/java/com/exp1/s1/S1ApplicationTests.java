8e0323e48379fa8f3ef42daff55bf3dbcee5a5de
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-10-22 01:55:07 -0300

    Archivos iniciales

---
+++ src/test/java/com/exp1/s1/S1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exp1.s1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
