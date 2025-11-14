9157e2e1b1ab91c3f28349570911d6917058d639
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-11-14 00:24:40 -0300

    Se corrige nombre de carpeta

--- src/main/java/com/exp1/s1/Controller/LibroController.java
+++ src/main/java/com/exp1/s1/Controller/LibroController.java
@@ -1,6 +1,5 @@
-package com.exp1.s1.Controller;
+package com.exp1.s1.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,40 +16,41 @@ import org.springframework.web.bind.annotation.GetMapping;
 import com.exp1.s1.model.Libro;
 import java.util.List;
 
-
-
 @RestController
 @RequestMapping("/libros")
 @CrossOrigin(origins = "*")
 public class LibroController {
 
-    @Autowired
-    private LibroService libroService;
+    private final LibroService libroService;
+
+    public LibroController(LibroService libroService) {
+        this.libroService = libroService;
+    }
 
     @GetMapping
-    public List<Libro> getAllLibros(){
+    public List<Libro> getAllLibros() {
         return libroService.getAllLibros();
     }
 
     @GetMapping("/{id}")
-    public Libro getLibroById(@PathVariable Long id){
+    public Libro getLibroById(@PathVariable Long id) {
         return libroService.getLibroById(id)
-            .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"Libro no encontrado con id: "+ id));
+                .orElseThrow(
+                        () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Libro no encontrado con id: " + id));
     }
 
     @PostMapping
-    public Libro createLibro(@RequestBody Libro libro){
+    public Libro createLibro(@RequestBody Libro libro) {
         return libroService.createLibro(libro);
     }
 
     @PutMapping("/{id}")
-    public Libro updateLibro(@PathVariable Long id, @RequestBody Libro libro){
+    public Libro updateLibro(@PathVariable Long id, @RequestBody Libro libro) {
         return libroService.updateLibro(id, libro);
     }
 
-
     @DeleteMapping("/{id}")
-    public void deleteLibro(@PathVariable Long id){
+    public void deleteLibro(@PathVariable Long id) {
         libroService.deleteLibro(id);
     }
 
